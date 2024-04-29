430ac29d9670ebbf4e06725b98bac09f691f5b00
Navid Ghorbanali <navidghorbanali@gmail.com>
2024-04-29 15:38:14 -0700

    first commit

---
+++ data_utilities.py
@@ -0,0 +1,172 @@
+""" This file contains Utility Functions, most of which are to prepare and manipulate the data. """
+
+
+from os import listdir
+from os.path import join
+import torch
+from PIL import Image
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import Compose, RandomCrop, ToTensor, ToPILImage, CenterCrop, Resize
+import torch.nn.functional as F
+from torch.autograd import Variable
+from math import exp
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
+
+def calculate_valid_crop_size(crop_size, upscale_factor):
+    return crop_size - (crop_size % upscale_factor)
+
+def train_hr_transform(crop_size):
+    return Compose([
+        RandomCrop(crop_size), 
+        ToTensor(), 
+    ])
+
+def train_lr_transform(crop_size, upscale_factor):
+    return Compose([
+        ToPILImage(), 
+        Resize(crop_size // upscale_factor, interpolation=Image.BICUBIC), 
+        ToTensor()
+    ])
+
+def display_transform():
+    return Compose([
+        ToPILImage(), 
+        Resize(400), 
+        CenterCrop(400), 
+        ToTensor(), 
+    ])
+
+
+class TrainDatasetFromFolder(Dataset):
+    def __init__(self, dataset_dir, crop_size, upscale_factor):
+        super(TrainDatasetFromFolder, self).__init__()
+        self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
+        crop_size = calculate_valid_crop_size(crop_size, upscale_factor)
+        self.hr_transform = train_hr_transform(crop_size)
+        self.lr_transform = train_lr_transform(crop_size, upscale_factor)
+
+    def __getitem__(self, index):
+        hr_image = self.hr_transform(Image.open(self.image_filenames[index]))
+        lr_image = self.lr_transform(hr_image)
+        return lr_image, hr_image
+    
+    def __len__(self):
+        return len(self.image_filenames)
+    
+
+class ValDatasetFromFolder(Dataset):
+    def __init__(self, dataset_dir, upscale_factor):
+        super(ValDatasetFromFolder, self).__init__()
+        self.upscale_factor = upscale_factor
+        self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
+
+    def __getitem__(self, index):
+        hr_image = Image.open(self.image_filenames[index])
+        w, h = hr_image.size
+        crop_size = calculate_valid_crop_size(min(w, h), self.upscale_factor)
+        lr_scale = Resize(crop_size // self.upscale_factor, interpolation=Image.BICUBIC)
+        hr_scale = Resize(crop_size, interpolation=Image.BICUBIC)
+        hr_image = CenterCrop(crop_size)(hr_image)
+        lr_image = lr_scale(hr_image)
+        hr_restore_img = hr_scale(lr_image)
+        return ToTensor()(lr_image), ToTensor()(hr_restore_img), ToTensor()(hr_image)
+    
+    def __len__(self):
+        return len(self.image_filenames)
+    
+
+class TestDatasetFromFolder(Dataset):
+    def __init__(self, dataset_dir, upscale_factor):
+        super(TestDatasetFromFolder, self).__init__()
+        self.lr_path = dataset_dir + "/SRF_" + str(upscale_factor) + "/data/"
+        self.hr_path = dataset_dir + "/SRF_" + str(upscale_factor) + '/target/'
+        self.upscale_factor = upscale_factor
+        self.lr_filenames = [join(self.lr_path, x) for x in listdir(self.lr_path) if is_image_file(x)]
+        self.hr_filenames = [join(self.hr_path, x) for x in listdir(self.hr_path) if is_image_file(x)]
+
+    def __getitem__(self, index):
+        image_name = self.lr_filenames[index].split('/')[-1]
+        lr_image = Image.open(self.lr_filenames[index])
+        w, h = lr_image.size
+        hr_image = Image.open(self.hr_filenames[index])
+        hr_scale = Resize((self.upscale_factor * h, self.upscale_factor * w), interpolation=Image.BICUBIC)
+        hr_restore_img = hr_scale(lr_image)
+        return image_name, ToTensor()(lr_image), ToTensor()(hr_restore_img), ToTensor()(hr_image)
+    
+    def __len__(self):
+        return len(self.lr_filenames)
+
+
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+    return gauss / gauss.sum()
+
+
+def create_window(window_size, channel):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    return window
+
+
+def _ssim(img1, img2, window, window_size, channel, size_average=True):
+    mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
+    mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
+    sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2, groups=channel) - mu1_mu2
+
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+
+    if size_average:
+        return ssim_map.mean()
+    else:
+        return ssim_map.mean(1).mean(1).mean(1)
+
+
+class SSIM(torch.nn.Module):
+    def __init__(self, window_size=11, size_average=True):
+        super(SSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = 1
+        self.window = create_window(window_size, self.channel)
+
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
+        else:
+            window = create_window(self.window_size, channel)
+
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+
+            self.window = window
+            self.channel = channel
+
+        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
+
+
+def ssim(img1, img2, window_size=11, size_average=True):
+    (_, channel, _, _) = img1.size()
+    window = create_window(window_size, channel)
+
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
+
+    return _ssim(img1, img2, window, window_size, channel, size_average)
